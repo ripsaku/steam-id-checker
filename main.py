d4ad8bc45a6e8886572697d469b24c998916c5f9
saku <zakupersonal@gmail.com>
2025-04-26 03:22:02 -0300

    Version 1.1
    New version of main.py and requirements.txt

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 import tkinter as tk
 from tkinter import filedialog
 import ctypes
+import datetime
 
 def set_console_title(title):
     ctypes.windll.kernel32.SetConsoleTitleW(title)
@@ -42,15 +43,29 @@ console.print(f"""\n\n
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 \n""", style="purple")
 
+console.print(f"[bold magenta][1][/bold magenta][magenta] Webhook (results through Discord messages)[/magenta]")
+console.print(f"[bold magenta][2][/bold magenta][magenta] No webhook (results exported into .txt)[/magenta]")
+console.print(f"[bold magenta]Choose mode: [/bold magenta]", end="")
 
-webhook = input("Discord Webhook: ")
+modo = input()
 
-r = requests.get(webhook)
-r.status_code
-if r.status_code == 401:
+if modo == '1':
+ webhook = input("Discord Webhook: ")
+ r = requests.get(webhook)
+ r.status_code
+ if r.status_code == 401:
     console.print("Invalid webhook", style="red")
     time.sleep(2.4)
     exit()
+
+elif modo == '2':
+    pass
+
+else:
+    console.print("Error: Invalid mode", style="red")
+    time.sleep(2)
+    exit()
+
 os.system("cls")
 
 root = tk.Tk()
@@ -69,7 +84,7 @@ try:
     with open(file_path, 'r') as archivo:
         palabras_a_probar = [line.strip() for line in archivo]
     total_palabras = len(palabras_a_probar)
-    print(f"Loaded {total_palabras} from file: {file_path}")
+    print(f"Loaded {total_palabras} words from file: {file_path}")
 except FileNotFoundError:
     console.print(f"Error: File not found on path: {file_path}", style="red")
     exit()
@@ -77,12 +92,33 @@ except Exception as e:
     console.print(f"Error reading the file: {e}", style="red")
     exit()
 
+results_folder = "Results"
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)
+
+now = datetime.datetime.now()
+timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+output_folder = os.path.join(results_folder, timestamp)
+os.makedirs(output_folder)
+
+invalid_file_path = os.path.join(output_folder, "INVALID.txt")
+valid_file_path = os.path.join(output_folder, "VALID.txt")
+
+invalid_file = open(invalid_file_path, 'w')
+valid_file = open(valid_file_path, 'w')
+
 palabras_checkeadas = 0
 
 for palabra in palabras_a_probar:
     palabras_checkeadas += 1
     if len(palabra) < 3:
         console.print(f'\r[bold yellow]{palabra}[/bold yellow] IS INVALID (too short)')
+        invalid_file.write(palabra + '\n')
+        invalid_file.flush()
+    elif len(palabra) > 32:
+        console.print(f'\r[bold yellow]{palabra}[/bold yellow] IS INVALID (too long)')
+        invalid_file.write(palabra + '\n')
+        invalid_file.flush()
     else:
         request = requests.get(f'https://steamcommunity.com/id/{palabra}')
         lxml = bs4.BeautifulSoup(request.content, 'lxml')
@@ -90,12 +126,22 @@ for palabra in palabras_a_probar:
         list_title = title.text.split()
         if list_title[-1] == "Error":
             console.print(f'\r[bold green]{palabra}[/bold green] IS AVAILABLE')
-            requests.post(webhook, data={"content" : f"\🌠 New ID available `{palabra}` \n remember **it can be banned** or **blacklisted.** >.<\n`--------------------------------`"})
+            valid_file.write(palabra + '\n')
+            valid_file.flush()
+            if modo == '1':
+                requests.post(webhook, data={"content" : f"\🌠 New ID available `{palabra}` \n remember **it can be banned** or **blacklisted.** >.<\n`--------------------------------`"})
+            elif modo == '2':
+                pass                 
         else:
             console.print(f'\r[bold red]{palabra}[/bold red] IS NOT AVAILABLE')
+            invalid_file.write(palabra + '\n')
+            invalid_file.flush()
     progress_text = f"IDs: {palabras_checkeadas}/{total_palabras}"
     set_console_title(progress_text)
     time.sleep(0.1)
+
+invalid_file.close()
+valid_file.close()
 
 console.print("\n\n\n")
 console.print(f'[bold magenta]Finished[/bold magenta]')
